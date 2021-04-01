66aa1a758f62365b1fcceb47a545be9616355d14
gyuree.kim <2017029334@hanyang.ac.kr>
2021-04-01 16:39:56 +0900

    Update isValid() and add getOnlyTimeWord()

--- sms_ner_pkg/sms_ner_pkg/time_detector.py
+++ sms_ner_pkg/sms_ner_pkg/time_detector.py
@@ -14,47 +14,80 @@ class TimeDetector():
         return self.times
 
     def addTime(self, word):
-        self.times.append(word)
+        if word: self.times.append(word)
 
     def hasNumbers(self, str):
         return any(char.isdigit() for char in str)
 
+    # 단어에 포함된 특수문자 제거
+    def deleteSpecialCharacters(self, word):
+        word = ''.join(char for char in word if char.isalnum())
+        return word
+
+    # 시간과 관련된 단어 외의 글자들은 삭제
+    def getOnlyTimeWord(self, _word):
+        word = []
+        def slice_list_by_targetWord(targetWord, word):
+            idx = word.index(targetWord)
+            return word[:idx+1]
+        def slice_list_from_targetWord(targetWord, word):
+            idx = word.index(targetWord)
+            return word[idx:]
+
+        #후, 뒤 -> 반 -> 분 -> 시 우선순위대로 자른다.
+        targetWords = ['후', '뒤', '반', '분', '시']
+        for targetWord in targetWords:
+            if targetWord in _word:
+                word = slice_list_by_targetWord(targetWord, _word)
+                break
+        # 첫번째 숫자 이전의 글자들 삭제
+        for character in _word:
+            if self.hasNumbers(character):
+                word = slice_list_from_targetWord(character, word)
+                break
+        return word
+
     # target이 포함된 word가 시간과 관련된 단어인지 유효성 검사
-    def isValid(self, target, word):
+    def isValid(self, word, idx):
         if word == '':
             return False
-        elif target == '시' or target == '분':
+        elif '시' in word or '분' in word:
             return self.hasNumbers(word)
-        elif target == '반' or target == '후' or target == '뒤':
-            return
-        return True
+        elif word in '반' or word in '후' or word in '뒤':
+            if self.hasNumbers(word) :
+                return True
+            elif self.words[idx-1] in self.times:
+                return True
+            else: return False
+        return False
 
     def time_detector(self, sentence):
         words = sentence.split(' ')
 
         # sentence 안에 시간과 관련된 단어만 times에 추가.
-        for i in range(len(words)):
-            word = words[i]
-            timeChecked = False
-
+        for word in words:
             #timeWords 단어 검사
-            for timeWord in timeWords:
+            for timeWord in self.timeWords:
                 if timeWord in word:
-                    timeChecked = True
-                    validValues[i] = True
-                    _time.append(word)
+                    _word = self.deleteSpecialCharacters(word)
+                    _word = self.getOnlyTimeWord(_word)
+                    self.addTime(_word)
             #targetWords 단어 검사
-            if not timeChecked:
-                for target in targetWords:
-                    if target in word:
-                        ################### sudo code #####################
-                        if '시' or '분': self.hasNumbers(word) ? append else continue
-                        elif '반' or '후' or '뒤' validValues[i-1] ? validValues[i]=True , append : continue
-        return _time
+            for targetWord in self.targetWords:
+                if targetWord in word:
+                    idx = words.index(word)
+                    if self.isValid(word,idx):
+                        _word = self.deleteSpecialCharacters(word)
+                        _word = self.getOnlyTimeWord(_word)
+                        self.addTime(_word)
+        return self.times
 
 if __name__ == "__main__":
-    messages = dataLoader._read_data_file()
+    os.chdir(r'C:\Users\ghio1\PycharmProjects\senior-project-2021\sms_ner_pkg\sms_ner_pkg\data')
+    current_path = os.getcwd()
+    messages = dataLoader.sms_data_loader(current_path)
     timeDetector = TimeDetector()
-    timeDetector.time_detector()
+    for message in messages:
+        timeDetector.time_detector(message)
     print(timeDetector.getTime())
 
